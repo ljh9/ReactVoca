6cf335ff6122f413f8df009a52d66c66ebe7b0e0
ljh9 <dkssud2208@naver.com>
2022-07-02 02:35:25 +0900

    React Map List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import Hello from "./component/Hello";
+import Day from './component/Day';
+import DayList from './component/DayList';
+// import Hello from "./component/Hello";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
-      <h3>props : properties</h3>
+      {/* <h3>props : properties</h3>
       <Hello age={10} />
       <Hello age={20} />
-      <Hello age={30} />
+      <Hello age={30} /> */}
+      <Header/>
+      <DayList/>
+      <Day/>
+
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>
+        토익 영단어(고급)
+      </h1>
+      <div className="menu">
+      </div>
+    </div>
+  );
+}
