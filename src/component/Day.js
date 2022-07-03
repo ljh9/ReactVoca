7f3129274414d3879d4f15f566ac55f9d582b409
ljh9 <dkssud2208@naver.com>
2022-07-04 01:05:44 +0900

    JsonServer, REST API, useEffect, fetch

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,22 +1,32 @@
-import dummy from "../db/data.json";
+// import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
+import Word from "./word";
+import { useEffect, useState } from "react";
 export default function Day(){
 
 //const day = 1;
 // const day = useParams().day;
 const {day} = useParams();
-const wordList = dummy.words.filter(word => word.day === Number(day));
+// const wordList = dummy.words.filter(word => word.day === Number(day));
+const [words, setWords] = useState([]);
+
+useEffect(()=>{
+    fetch(`http://localhost:3001/words?day=${day}`)
+    .then(res => {
+        return res.json()
+    })
+    .then(data =>{
+        setWords(data);
+    })
+}, [day]);
 
     return(
         <>
         <h2>Day{day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word => (
-                        <tr key={word.id}>
-                            <td>{word.eng}</td>
-                            <td>{word.kor}</td>
-                        </tr>
+                    {words.map(word => (
+                        <Word word={word} key={word.id}/>
                     ))}
                 </tbody>
             </table>
