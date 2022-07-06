5c917426b748f26928e127579e0c1adceb68cd4f
ljh9 <dkssud2208@naver.com>
2022-07-07 00:32:30 +0900

    React REST API POST, useNavigate

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import DayList from './component/DayList';
 import Header from "./component/Header";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import EmptyPage from './component/EmptyPage';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
 
 
 function App() {
@@ -21,6 +23,8 @@ function App() {
         <Routes>
           <Route path="/"  element={<DayList/>} />
           <Route path="/day/:day" element={<Day/>} />
+          <Route path="/create_word" element={<CreateWord/>} />
+          <Route path="/create_day" element={<CreateDay/>} />
           <Route path='*' element={<EmptyPage/>} />
         </Routes>
     </div>
