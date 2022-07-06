3f84d04a0d7a2a55ea88a3207077c17822a0a891
ljh9 <dkssud2208@naver.com>
2022-07-07 00:32:43 +0900

    React REST API POST, useNavigate

---
+++ src/component/CreateDay.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import useFetch from "../hooks/useFetch";
+import { useNavigate} from 'react-router-dom';
+
+export default function CreateDay(){
+    const days = useFetch("http://localhost:3001/days");
+    const navigate = useNavigate();
+
+    function addDay(e){
+        e.preventDefault();
+
+        fetch(`http://localhost:3001/days/`, {
+            method : "POST", 
+            headers : {
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify({
+                day : days.length + 1
+            }),
+        }).then(res => {
+            if(res.ok){
+                alert("생성 완료");
+                navigate(`/`);
+            }
+        });
+    }
+
+    return <div>
+        <h3>현재 일수 : {days.length}일</h3>
+        <button onClick={addDay}>Day 추가</button>
+    </div>;
+}
